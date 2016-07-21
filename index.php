99aaf097786f643ca24a8a57ad7ffac0e6354f7e
PavelBabakin <pavelbabakin7@gmail.com>
2016-07-21 12:34:56 +0300

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>File Upload</title>
+</head>
+<body>
+    <p>Hello</p>
+    <?php
+        echo "World";
+    ?>
+</body>
+</html>
